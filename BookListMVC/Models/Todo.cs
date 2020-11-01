ab5b85e0244f1e905a79548df682f73d59de4187
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-11-01 16:34:47 -0600

    staged todo commits

---
+++ BookListMVC/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListMVC.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Value { get; set; }
+        public bool Completed { get; set; }
+    }
+}
